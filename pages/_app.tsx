b288510ba1d5b2eabcf5f16000046e664147166d
Colanns <mycolands@gmail.com>
2025-08-02 20:14:56 +0800

    feat: 删除无用代码，修改 favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>✨ 魔法少女生成器 ✨</title>
         <meta name="description" content="为你生成独特的魔法少女角色" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
     </>
