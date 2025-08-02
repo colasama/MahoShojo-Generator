f9d7014410c426c682e32a1c2f17b8284e800a92
Colanns <mycolands@gmail.com>
2025-08-03 02:57:37 +0800

    feat: 添加 Vercel 分析器

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '@/styles/globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 } 
