5f8b343c9f42462e0028eb9e471b04423ec7b9ea
Colanns <mycolands@gmail.com>
2025-08-09 01:52:19 +0800

    feat: 修改预设命名，避免中文无法读取

--- pages/api/get-presets.ts
+++ pages/api/get-presets.ts
@@ -26,57 +26,57 @@ const PRESET_LIST: PresetMagicalGirl[] = [
   {
     name: "翠雀",
     description: "樊笼下的蓝翠雀：身经百战的前辈，外冷内热的魔法少女。",
-    filename: "翠雀·笼中雀.json"
+    filename: "centaurea.json"
   },
   {
     name: "白玫",
     description: "小草包：渴望认可的理想主义者，以“翠雀”为目标努力的成长型新人。",
-    filename: "白玫·草包.json"
+    filename: "white_rose.json"
   },
   {
     name: "小锦",
-    description: "拿最多的信息，打最少的输出：天赋异禀但缺乏安全感的魔法少女，渴望真正的‘家’。",
-    filename: "小锦·情报网.json"
+    description: "拿最多的信息，打最少的输出：天赋异禀但缺乏安全感的魔法少女，渴望真正的'家'。",
+    filename: "little_brocade.json"
   },
   {
     name: "薄雪",
     description: "野兽心境：以治愈之力行复仇之事的战斗天才。",
-    filename: "薄雪·以野兽的心境.json"
+    filename: "thin_snow.json"
   },
   {
     name: "麻雀",
     description: "牢雀：正被关在调查院地牢里承受挠痒痒酷刑。",
-    filename: "麻雀·爪痕.json"
+    filename: "sparrow.json"
   },
   {
     name: "玛格丽特",
-    description: "调酒师：以情绪为武器的万能‘润滑剂’，张扬自信的调查院前辈。",
-    filename: "玛格丽特·酌情热醉.json"
+    description: "调酒师：以情绪为武器的万能'润滑剂'，张扬自信的调查院前辈。",
+    filename: "margaret.json"
   },
   {
     name: "朝颜",
-    description: "祖母绿的科技与狠活：背负他人身影的‘记录者’，活在悔恨与爱恋中的败犬，让人怀疑她是不是有在吃代餐。",
-    filename: "朝颜·科技花牌.json"
+    description: "祖母绿的科技与狠活：背负他人身影的'记录者'，活在悔恨与爱恋中的败犬，让人怀疑她是不是有在吃代餐。",
+    filename: "morning_glory.json"
   },
   {
     name: "鸢",
     description: "爪痕兽心：只相信自身技艺的武痴，行走于阴影中的反权威者。",
-    filename: "鸢·银屏山之战.json"
+    filename: "kite.json"
   },
   {
     name: "松花",
     description: "圣地巡礼：热衷圣地巡礼的摸鱼少女，能将回忆凝固为琥珀。",
-    filename: "松花·圣地巡礼.json"
+    filename: "pine_flower.json"
   },
   {
     name: "艾草",
     description: "圣地巡礼：言出法随的靠谱魔法少女，能将记录的话语化为力量。",
-    filename: "艾草·言出必行.json"
+    filename: "mugwort.json"
   },
   {
     name: "向日葵",
     description: "圣地巡礼：追逐大新闻的乐子人，能将照片中的景象再现。",
-    filename: "向日葵·旧景重现.json"
+    filename: "sunflower.json"
   }
   // 如果有更多预设，可以像下面这样继续添加：
   // {
