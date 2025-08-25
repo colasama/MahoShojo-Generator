419981201d303e0a23767cb2a539d42fa9b1797a
末伏之夜 <notuhao@qq.com>
2025-08-25 08:47:39 +0800

    feat: 为情景文件增加前缀

--- pages/scenario.tsx
+++ pages/scenario.tsx
@@ -89,7 +89,7 @@ const ScenarioPage: React.FC = () => {
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.download = `${title}.json`;
+    link.download = `情景_${title}.json`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
